fbff490175994f4849299b080ca85ef85f1ab018
ganondorf7 <66956603+ganondorf7@users.noreply.github.com>
2023-12-22 13:31:16 +0000

    video addded

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function hidef() {
+  var x = document.getElementById("video");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
